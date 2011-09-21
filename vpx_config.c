3b15f555708aec7445b59e6ae9c04496e49e521a
rikaunite <jiongtl@gmail.com>
2011-09-21 08:27:32 +0800

    Add some generated files.

---
+++ vpx_config.c
@@ -0,0 +1,2 @@
+static const char* const cfg = "--target=x86-win32-vs9 --disable-vp8-encoder --disable-examples --disable-install-docs --enable-static-msvcrt --enable-runtime-cpu-detect";
+const char *vpx_codec_build_config(void) {return cfg;}
